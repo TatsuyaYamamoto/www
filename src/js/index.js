b150ce483eeeba2f1520cebfececc6f4f4f7a31b
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2018-01-11 12:42:30 +0900

    fix, can't toggel sidenav in index page

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,15 @@ import 'materialize-css/js/cash';
 import 'materialize-css/js/parallax';
 import 'materialize-css/js/anime.min';
 import 'materialize-css/js/scrollspy';
+import 'materialize-css/js/sidenav';
 
 const aboutElement = document.getElementById('about');
 new M.ScrollSpy(aboutElement);
 
+const sidenav = document.querySelector('.sidenav');
+new M.Sidenav(sidenav);
+
 const parallaxes = document.querySelectorAll('.parallax');
 
 Array.from(parallaxes).forEach((p) => new M.Parallax(p));
+
