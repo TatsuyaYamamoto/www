3710e17265dfede6a66c634a281dff9f0d3b9c30
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2018-01-18 12:40:25 +0900

    arrange layout link tab

--- src/js/activity.js
+++ src/js/activity.js
@@ -1,7 +1,3 @@
 import 'materialize-css/js/global';
 import 'materialize-css/js/cash';
 import 'materialize-css/js/anime.min'
-import 'materialize-css/js/sidenav';
-
-const sidenav = document.querySelector('.sidenav');
-new M.Sidenav(sidenav);
--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,9 @@ import 'materialize-css/js/cash';
 import 'materialize-css/js/parallax';
 import 'materialize-css/js/anime.min';
 import 'materialize-css/js/scrollspy';
-import 'materialize-css/js/sidenav';
 
 const aboutElement = document.getElementById('about');
 new M.ScrollSpy(aboutElement);
-
-const sidenav = document.querySelector('.sidenav');
-new M.Sidenav(sidenav);
 
 const parallaxes = document.querySelectorAll('.parallax');
 
