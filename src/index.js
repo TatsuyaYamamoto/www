f6b33d5c1d4d35ad18e9d53b748ea797f68f7091
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2018-07-03 18:53:45 +0900

    fix, protruded tab bar items

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "./index.scss";
+
+import { MDCTabBarScroller } from "@material/tabs";
+
+function init() {
+  const tabBarScroller = document.querySelector("#my-mdc-tab-bar-scroller");
+  MDCTabBarScroller.attachTo(tabBarScroller);
+}
+
+window.addEventListener("load", init);
