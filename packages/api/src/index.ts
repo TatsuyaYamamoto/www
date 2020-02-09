f9473e8549b8152b44d3f41b953d34c8e1bcfa13
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2020-02-09 20:05:26 +0900

    feat(api): trigger youtube comment caching with scheduler

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,3 +2,5 @@ import { https } from "firebase-functions";
 import _httpsApp from "./https";
 
 export const httpsApp = https.onRequest(_httpsApp);
+
+export { cacheYoutubeComments } from "./pubsub/schedule";
